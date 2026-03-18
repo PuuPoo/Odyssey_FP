a98ad795d7c050d00bbb8c78bcfceea492aa267f
PuuPoo <Louischristian.wibowo@gmail.com>
2026-03-18 23:45:59 +0700

    Added the texfield functionality
    Signed-off-by: PuuPoo <Louischristian.wibowo@gmail.com>

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,11 +1,16 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -14,6 +19,13 @@ public class Controller {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private PasswordField passwordTextField;
+    @FXML
+    private Label loginMessage;
 
     public void switchToHomeLoginPage(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("../views/homeLogin/HomeLoginPage.fxml"));
@@ -38,5 +50,15 @@ public class Controller {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+
+    public void doctorLoginButtonOnAction(ActionEvent e){
+        if(!usernameTextField.getText().isBlank() && !passwordTextField.getText().isBlank()){
+            loginMessage.setText("Logging in");
+        }
+        else{
+            loginMessage.setText("nope");
+        }
     }
 }
